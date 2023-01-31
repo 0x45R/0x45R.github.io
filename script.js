96f5cb6c9be134df17647fd94484e71807ea217f
0x45R <74537691+0x45R@users.noreply.github.com>
2023-01-31 22:24:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ class Carousel{
      <div class="row">
         <div class="row">
           <i class="ti ti-arrow-big-left"></i>
-          <i class="ti ti-arrow-bar-left"></i>
+          <i class="ti ti-arrow-big-left-lines"></i>
         </div>
         <div class="slides">
         </div>
         <div class="row">
           <i class="ti ti-arrow-bar-right"></i>
-          <i class="ti ti-arrow-big-right"></i>
+          <i class="ti ti-arrow-big-right-lines"></i>
         </div>
         </div>
         <div class="row">
