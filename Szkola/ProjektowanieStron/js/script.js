3a07e3691926ba3a777f43788fe14589045b4f0e
0x45R <robertplawski8@gmail.com>
2023-02-10 20:32:40 +0100

    Upgraded Szkola/ProjektowanieStron

---
+++ Szkola/ProjektowanieStron/js/script.js
@@ -0,0 +1,2 @@
+const navigation_buttons = document.querySelectorAll(".navigation-button")
+navigation_buttons.forEach((current) => {current.addEventListener("click",()=>{alert("Nie chce mi się robić przycisków :c")})})
